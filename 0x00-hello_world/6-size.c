332368d1b53a200bcea8c47b2dfbaf4a3f2c698c
fira <fraoldeti@gmail.com>
2022-12-08 14:39:22 -0800

    size of type

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	char c;
+	long int li;
+	long long int lli;
+	float f;
+
+	printf("size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("size of a int: %lu byte(s)\n", (unsigned long)sizeof(i));
+	printf("size of a long int: %lu byte(s)\n", (unsigned long)sizeof(li));
+	printf("size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(lli));
+	printf("size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+	return (0);
+}
