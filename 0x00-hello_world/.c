74c4473d90603d45e087ff8f1c44f86e5562d62f
fira <fraoldeti@gmail.com>
2022-12-08 12:36:46 -0800

    preprocessor step

---
+++ 0x00-hello_world/.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ *  * main - Entry point
+ *   *
+ *    * Return: Always 0 (Success)
+ *     */
+int main(void)
+{
+return(0);
+}
+#include <stdio.h>
+/**
+ *  *  * main - Entry point
+ *   *   *
+ *    *    * Return: Always 0 (Success)
+ *     *     */
+int main(void)
+{
+	return(0);
+}#include <stdio.h>
+/**
+ *  *  * main - Entry point
+ *   *   *
+ *    *    * Return: Always 0 (Success)
+ *     *     */
+int main(void)
+{
+	return(0);
+}
---
+++ 0x00-hello_world/README.md
@@ -0,0 +1,5 @@
+WordPress
+:  let start with c programming
+
+Markdown
+:  my first task is the source code only execute in preprocessor step  
