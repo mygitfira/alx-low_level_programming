976a48b8acb6e2145e78e4fdffc4a9a16ddab64f
fira <fraoldeti@gmail.com>
2022-12-09 12:41:55 -0800

    display decimal using putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int c = 48;
+	while (c <= 57)
+	{
+		putchar(c);
+		c++;
+	}
+	putchar(10);
+return (0);
+}
