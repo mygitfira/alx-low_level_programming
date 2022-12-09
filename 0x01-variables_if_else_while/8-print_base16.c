ff096b3cf94a0128c4a8274a1fee342a97275bf0
fira <fraoldeti@gmail.com>
2022-12-09 12:59:19 -0800

    hexadecimal number

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int c = 48, m = 97;
+	while (c <= 57)
+	{
+		putchar(c);
+		c++;
+	}
+while (m <= 102)
+{
+putchar(m);
+	m++;
+	}
+	putchar(10);
+return (0);
+}
