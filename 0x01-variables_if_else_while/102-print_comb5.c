fc71e75e473c8b4a84895c9c3878307a106ed99a
fira <fraoldeti@gmail.com>
2022-12-11 23:53:21 -0800

    two two digit combination

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int c, m, l, p;
+	for (c = 48; c <= 57; c++)
+	{
+		for (m = 48; m <= 57; m++)
+		{
+			for (l = 48; l <= 57; l++)
+			{
+				for (p = 48; p <= 57; p++)
+				{
+					if (c <= l && m < p)
+					{
+					putchar(c);
+					putchar(m);
+					putchar(32);
+					putchar(l);
+					putchar(p);
+					putchar(44);
+					}
+			}
+		}
+		}
+	}
+	putchar(10);
+return (0);
+}
