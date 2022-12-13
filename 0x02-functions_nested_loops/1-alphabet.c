ddcd0d08a1ce97961635b27ae24205d6639c7504
fira <fraoldeti@gmail.com>
2022-12-13 11:34:42 -0800

    print lowercase alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *main - Entry point
+ *
+ *Return: Always 0 (Success)
+ */
+
+void print_alphabet(void)
+{
+	char i = 'a';
+	while (i <= 'z')
+	{
+		_putchar(i);
+		i++;
+	}
+	_putchar('\n');
+}
