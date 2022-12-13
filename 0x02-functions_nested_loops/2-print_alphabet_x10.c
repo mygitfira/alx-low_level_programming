a843c3179eba284158f7e1be9201dcb6fbcd31b0
fira <fraoldeti@gmail.com>
2022-12-13 11:58:46 -0800

    print loweracse alphabet 10 times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,18 +1,24 @@
 #include "main.h"
 
 /**
- *print_alphabet - Entry point
+ *print_alphabet_x10 - Entry point
  *
  *Return: Always 0 (Success)
  */
 
-void print_alphabet(void)
+void print_alphabet_x10(void)
 {
 char i = 'a';
+int j = 1;
+	while (j <= 10)
+	{
+	i = 'a';
 	while (i <= 'z')
 	{
 		_putchar(i);
 		i++;
 	}
 	_putchar('\n');
+	j++;
+	}
 }
