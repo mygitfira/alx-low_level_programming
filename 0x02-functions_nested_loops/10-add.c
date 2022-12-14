526ed3b4be32dd0960441b6335a4c35bcd3735c3
fira <fraoldeti@gmail.com>
2022-12-14 13:51:12 -0800

    add two integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *add - Entry point
+ *@i: character to be checked
+ *@j: character to be checked
+ *Return: Always 0 (Success)
+ */
+
+int add(int i, int j)
+{
+int k = i + j;
+return (k);
+}
