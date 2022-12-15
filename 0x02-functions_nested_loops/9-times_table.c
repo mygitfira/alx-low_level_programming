322631e2127c9ec0964a33b22ab6fa3efc3b32f5
fira <fraoldeti@gmail.com>
2022-12-15 06:42:11 -0800

    natural number

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,17 +11,20 @@ void times_table(void)
 int i, j, k;
 for (i = 0; i <= 9; i++)
 {
-	_putchar('0');
 	for (j = 0; j <= 9; j++)
 	{
 		k = j * i;
+		if (k <= 9)
+		{
+		_putchar(k + '0');
+		_putchar(' ');
+		}
+		else
+		{
+		_putchar(k + '0');
+		}
 		_putchar(',');
 		_putchar(' ');
-		if (k <= 9)
-		_putchar(' ');
-		else
-			_putchar((k / 10) + '0');
-		_putchar((k % 10) + '0');
 	}
 	_putchar('\n');
 }
