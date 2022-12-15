d11deb1e2291b1097f73c0ddbaedb948d1858667
fira <fraoldeti@gmail.com>
2022-12-15 12:18:22 -0800

    check for uppercase

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *main - check the code.
+ *
+ *Return: Always 0.
+ */
+
+int _isupper(char c)
+{
+	if (c >= 'A')
+		return (1);
+	else
+		return (0);
+}
