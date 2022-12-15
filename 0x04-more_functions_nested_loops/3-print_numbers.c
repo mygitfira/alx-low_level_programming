3371863e932bf109634e2243740c21a34c0bd1dc
fira <fraoldeti@gmail.com>
2022-12-15 13:15:37 -0800

    print 0-9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *print_numbers - check the code.
+ *
+ *Return: void
+ */
+
+void print_numbers(void)
+{
+	char i = 48;
+	while (i < 58)
+	{
+		_putchar(i);
+		i++;
+	}
+	_putchar('\n');
+}
