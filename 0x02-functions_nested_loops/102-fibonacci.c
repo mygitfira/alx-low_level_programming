0e2499c669ba353fa7ac6d37e7e2ec87d24df82f
fira <fraoldeti@gmail.com>
2022-12-17 12:02:14 -0800

    hot fix

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -8,15 +8,15 @@
 int main(void)
 {
 int i, sum = 2, j = 1, k, l;
-printf("%d, ", j);
+printf("%d, %d, ", j, sum);
 	for (i = 1; i < 50; i++)
 	{
 		k = sum;
 		l = j;
-		printf("%d, ", sum);
 		sum = sum + j;
 		if (k > l)
 			j = k;
+		printf("%d, ", sum);
 	}
 	printf("\n");
 	return (0);
