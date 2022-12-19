02abf458ad9ec33852fbde6131d5b9940936a710
fira <fraoldeti@gmail.com>
2022-12-19 15:30:14 -0800

    reverse string

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_rev - check the code
+ * @s: parametre s
+ * Return: aleays k.
+ */
+
+void print_rev(char *s)
+{
+int i = 0;
+while (s[i] != '\0')
+{
+i++;
+}
+for (i = i - 1; i >= 0; i--)
+{
+	_putchar(s[i]);
+}
+_putchar('\n');
+}
