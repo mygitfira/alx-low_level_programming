f53baccf1d3321c1b1fdfb80458f752b575591e7
fira <fraoldeti@gmail.com>
2022-12-20 11:54:57 -0800

    hot fix

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -13,9 +13,19 @@ while (str[i] != '\0')
 {
 i++;
 }
+if (i % 2 == 0)
+{
 for (j = i / 2; j < i; j++)
 {
 	_putchar(str[j]);
+}
+}
+else
+{
+for (j = (i - 1) / 2; j < i; j++)
+{
+_putchar(str[j]);
+}
 }
 _putchar('\n');
 }
