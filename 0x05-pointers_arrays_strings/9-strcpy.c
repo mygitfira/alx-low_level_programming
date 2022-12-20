5b1a1f9fe6750a50f60e50e5b08cf95722ace7f8
fira <fraoldeti@gmail.com>
2022-12-20 12:40:37 -0800

    copy string

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *_strcpy - check the code
+ *@dest: parametre a
+ *@src: parameter b
+ *Return: dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+int i = 0, j;
+while (src[i] != '\0')
+{
+i++;
+}
+	for (j = 0; j <= (i + 1); j++)
+	{
+		dest[j] = src[j];
+	}
+return (dest);
+}
