d341572c582571500e794930c8facf4ae60b6f3f
fira <fraoldeti@gmail.com>
2022-12-21 14:39:28 -0800

    capitalization

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,47 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * cap_string - capitalize function
+ * @a: string 1.
+ *
+ * Return: dest.
+ */
+char *cap_string(char *a)
+{
+int len = 0, i, j;
+char k = 'a', l = 'A';
+while (a[len] != '\0')
+{
+	len++;
+}
+for (i = 0; i <= len; i++)
+{
+	k = 'a';
+	l = 'A';
+	if (a[i] == ' ' || a[i] == '\n' || a[i] == ',' || a[i] == ';' || a[i] == '.' || a[i] == '!' || a[i] == '?' || a[i] == '"' || a[i] == '(' || a[i] == ')' || a[i] == '{' || a[i] == '}' || a[i] == '\t')
+	{
+		if (a[i] == '\t')
+			a[i] = ' ';
+		for (j = 0; j < 26; j++)
+		{
+			if (a[i + 1] == ' ')
+			{
+				l = 'A';
+				k = 'a';
+			}
+			else if (a[i + 1] == k || a[i + 1] == l)
+			{
+				a[i + 1] = l;
+				k = 'a';
+				l = 'A';
+			}
+			else
+			{
+				k++;
+				l++;
+			}
+		}
+	}
+}
+return (a);
+}
