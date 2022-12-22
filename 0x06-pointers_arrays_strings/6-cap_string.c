e9e266b4ed78178f63dabf70b0ef81d9d48386c1
fira <fraoldeti@gmail.com>
2022-12-22 02:43:19 -0800

    hot fix

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,39 +8,30 @@
  */
 char *cap_string(char *a)
 {
-int len = 0, i, j;
-char k = 'a', l = 'A';
-while (a[len] != '\0')
+int i, j, trigger;
+char nots[] = ",;.!?(){}\n\t\" ";
+for (i = 0, trigger = 0; a[i] != '\0'; i++)
 {
-	len++;
-}
-for (i = 0; i <= len; i++)
-{
-	k = 'a';
-	l = 'A';
-	if (a[i] == ' ' || a[i] == '\n' || a[i] == ',' || a[i] == ';' || a[i] == '.' || a[i] == '!' || a[i] == '?' || a[i] == '"' || a[i] == '(' || a[i] == ')' || a[i] == '{' || a[i] == '}' || a[i] == '\t')
+	for (j = 0; nots[j] != '\0'; j++)
 	{
+		if (nots[j] == a[i])
+			trigger = 1;
 		if (a[i] == '\t')
 			a[i] = ' ';
-		for (j = 0; j < 26; j++)
+	}
+	if (trigger)
+	{
+		if (a[i + 1] == ' ')
+			trigger = 0;
+		if (a[i + 1] > 96 && a[i + 1] < 123)
 		{
-			if (a[i + 1] == ' ')
-			{
-				l = 'A';
-				k = 'a';
-			}
-			else if (a[i + 1] == k || a[i + 1] == l)
-			{
-				a[i + 1] = l;
-				k = 'a';
-				l = 'A';
-			}
-			else
-			{
-				k++;
-				l++;
-			}
+			a[i + 1] -= 32;
+			trigger = 0;
 		}
+		else if (a[i + 1] > 64 || a[i + 1] < 91)
+			trigger = 0;
+		else if (a[i + 1] > 47 || a[i + 1] < 58)
+			trigger = 0;
 	}
 }
 return (a);
