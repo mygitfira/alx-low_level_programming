26606bb980c40ec6cbad99800b8f9c20cb0d6665
fira <fraoldeti@gmail.com>
2023-01-02 07:21:47 -0800

    after occurence of l print string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strchr - prints buffer in hexa
+ * @s: the address of memory to print
+ * @c: the size of the memory to print
+ * Return: 0.
+ */
+char *_strchr(char *s, char c)
+{
+int i = 0, j;
+while (s[i])
+{
+	i++;
+}
+		for (j = 0; j <= i; j++)
+		{
+			if (s[j] == c)
+			{
+			s += j;
+			}
+		}
+return (s);
+}
