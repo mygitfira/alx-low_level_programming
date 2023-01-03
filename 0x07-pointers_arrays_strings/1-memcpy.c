699b0398dbf79673594db31de7f0235ceac74c0f
fira <fraoldeti@gmail.com>
2023-01-02 23:00:25 -0800

    hot fix

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -13,10 +13,7 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 unsigned int i;
 for (i = 0; i < n; i++)
 {
-	if (i != 5)
 	dest[i] = src[i];
-	else
-		dest[i] = src[i + 1];
 }
 return (dest);
 }
