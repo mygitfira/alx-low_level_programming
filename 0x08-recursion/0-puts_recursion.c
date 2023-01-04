79d6a0d7d8cf8a4939f152b1b8c8684f809633b2
fira <fraoldeti@gmail.com>
2023-01-04 09:05:23 -0800

    hot fix

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,19 +1,19 @@
 #include "main.h"
+
 /**
  * _puts_recursion - function recurtion
  * @s: string
- *
+ * Return:
  */
 
 void _puts_recursion(char *s)
 {
-int i = 0;
-_putchar(s[i]);
-if (s[i] != '\0')
-{
-	i++;
-	_puts_recursion(s + i);
-	if (s[i] == '\0')
+	if (*s == '\0')
+	{
 		_putchar('\n');
-}
+		return;
+	}
+	_putchar(*s);
+	s++;
+	_puts_recursion(s);
 }
