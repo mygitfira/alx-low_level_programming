17b32501ad6e017a99cfd60d8532e0b12fffeef5
fira <fraoldeti@gmail.com>
2023-01-06 13:04:31 -0800

    prints program name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - main function
+ * @argc: size of array
+ * @argv: arguents
+ * Return: 0.
+ */
+
+int main (__attribute__((unused)) int argc, char *argv[])
+{
+printf("%s\n", argv[0]);
+return 0;
+}
