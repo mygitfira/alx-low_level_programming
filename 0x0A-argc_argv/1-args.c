4f1e01d4172deb09e36e6b7a67d501d553560c86
fira <fraoldeti@gmail.com>
2023-01-06 13:18:46 -0800

    count arguments

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -6,8 +6,8 @@
  * Return: 0.
  */
 
-int main(__attribute__((unused)) int argc, char *argv[])
+int main(int argc, __attribute__((unused)) char *argv[])
 {
-printf("%s\n", argv[0]);
+printf("%d\n", argc - 1);
 return (0);
 }
